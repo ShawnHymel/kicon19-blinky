9a4edd8146d7bc14d105afc6bee3b1df9fe0756f
ShawnHymel <hymelsr@vt.edu>
2019-04-07 07:55:36 -0500

    Updated Arduino code for more linear blink

--- fade_blinky/fade_blinky.ino
+++ fade_blinky/fade_blinky.ino
@@ -9,8 +9,18 @@
 const int LED0 = 0;
 const int LED1 = 1;
 
-const int WAIT_TIME = 10;
-const int OFF_TIME = 1000;
+const int WAIT_TIME = 50;     // Time (ms) between LED updated
+const int OFF_TIME = 1000;    // Time (ms) LED is off
+
+// Perceived linear fade scale
+const uint8_t PWM_TABLE_SIZE = 30;
+const uint8_t pwm_table[PWM_TABLE_SIZE] PROGMEM = { 
+                                        0,   1,   2,   3,   4,
+                                        5,   6,   7,   8,   9,
+                                       10,  11,  14,  16,  19,  
+                                       23,  27,  32,  38,  45,  
+                                       54,  64,  76,  90, 107, 
+                                      128, 151, 180, 214, 255}; 
 
 void setup() {
   pinMode(LED0, OUTPUT);
@@ -18,15 +28,18 @@ void setup() {
 }
 
 void loop() {
-  for ( int i = 0; i <= 255; i++ ) {
-    analogWrite(LED0, i);
-    analogWrite(LED1, i);
+
+  // Fade on
+  for ( int8_t i = 0; i < PWM_TABLE_SIZE; i++ ) {
+    analogWrite(LED0, pgm_read_byte(&pwm_table[i]));
+    analogWrite(LED1, pgm_read_byte(&pwm_table[i]));
     delay(WAIT_TIME);
   }
-  
-  for ( int i = 255; i >= 0; i-- ) {
-    analogWrite(LED0, i);
-    analogWrite(LED1, i);
+
+  // Fade off
+  for ( int8_t i = PWM_TABLE_SIZE - 1; i >= 0; i-- ) {
+    analogWrite(LED0, pgm_read_byte(&pwm_table[i]));
+    analogWrite(LED1, pgm_read_byte(&pwm_table[i]));
     delay(WAIT_TIME);
   }
 
